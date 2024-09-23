ca6a7d3c4aa7a8770fb24cc7d92f0dc67d190db0
Kevin Sheehan <kshhn12@gmail.com>
2024-09-22 21:52:52 -0400

    updated so set limit form is in separate file

--- ProcessLimiterManager/SetLimitForm.cs
+++ ProcessLimiterManager/SetLimitForm.cs
@@ -1,51 +1,48 @@
 ﻿namespace ProcessLimiterManager
 {
-    public partial class MainForm
+    public class SetLimitForm : Form
     {
-        public class SetLimitForm : Form
+        private TextBox txtWarningTime;
+        private TextBox txtKillTime;
+
+        public string WarningTime { get; private set; }
+        public string KillTime { get; private set; }
+
+        public SetLimitForm(string processName, string currentWarningTime, string currentKillTime)
         {
-            private TextBox txtWarningTime;
-            private TextBox txtKillTime;
+            Text = $"Set Limits for {processName}";
 
-            public string WarningTime { get; private set; }
-            public string KillTime { get; private set; }
+            var lblWarning = new Label { Text = "Warning Time (hh:mm:ss):", Left = 10, Top = 10 };
+            txtWarningTime = new TextBox { Text = currentWarningTime, Left = 10, Top = 30, Width = 100 };
 
-            public SetLimitForm(string processName, string currentWarningTime, string currentKillTime)
+            var lblKill = new Label { Text = "Kill Time (hh:mm:ss):", Left = 10, Top = 60 };
+            txtKillTime = new TextBox { Text = currentKillTime, Left = 10, Top = 80, Width = 100 };
+
+            var btnOk = new Button { Text = "OK", Left = 10, Top = 110, DialogResult = DialogResult.OK };
+            btnOk.Click += BtnOk_Click;
+
+            var btnCancel = new Button { Text = "Cancel", Left = 100, Top = 110, DialogResult = DialogResult.Cancel };
+
+            Controls.AddRange(new Control[] { lblWarning, txtWarningTime, lblKill, txtKillTime, btnOk, btnCancel });
+
+            AcceptButton = btnOk;
+            CancelButton = btnCancel;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            StartPosition = FormStartPosition.CenterParent;
+            Size = new System.Drawing.Size(250, 200);
+        }
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            if (txtWarningTime.Text != null && txtKillTime.Text != null)
             {
-                Text = $"Set Limits for {processName}";
-
-                var lblWarning = new Label { Text = "Warning Time (hh:mm:ss):", Left = 10, Top = 10 };
-                txtWarningTime = new TextBox { Text = currentWarningTime, Left = 10, Top = 30, Width = 100 };
-
-                var lblKill = new Label { Text = "Kill Time (hh:mm:ss):", Left = 10, Top = 60 };
-                txtKillTime = new TextBox { Text = currentKillTime, Left = 10, Top = 80, Width = 100 };
-
-                var btnOk = new Button { Text = "OK", Left = 10, Top = 110, DialogResult = DialogResult.OK };
-                btnOk.Click += BtnOk_Click;
-
-                var btnCancel = new Button { Text = "Cancel", Left = 100, Top = 110, DialogResult = DialogResult.Cancel };
-
-                Controls.AddRange(new Control[] { lblWarning, txtWarningTime, lblKill, txtKillTime, btnOk, btnCancel });
-
-                AcceptButton = btnOk;
-                CancelButton = btnCancel;
-                FormBorderStyle = FormBorderStyle.FixedDialog;
-                StartPosition = FormStartPosition.CenterParent;
-                Size = new System.Drawing.Size(250, 200);
+                WarningTime = txtWarningTime.Text;
+                KillTime = txtKillTime.Text;
             }
-
-            private void BtnOk_Click(object sender, EventArgs e)
+            else
             {
-                if (txtWarningTime.Text != null && txtKillTime.Text != null)
-                {
-                    WarningTime = txtWarningTime.Text;
-                    KillTime = txtKillTime.Text;
-                }
-                else
-                {
-                    MessageBox.Show("Invalid time format. Please use hh:mm:ss.");
-                    DialogResult = DialogResult.None;
-                }
+                MessageBox.Show("Invalid time format. Please use hh:mm:ss.");
+                DialogResult = DialogResult.None;
             }
         }
     }
